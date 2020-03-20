de590b3d5545264ca4320f8639638b8422dac50b
Your Name <itokanchi@itouhiroshichinoMacBook-ea.local>
2020-03-20 22:20:46 +0900

    Modified templates to display posts from database

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Post(models.Model):
     auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    text = models.TextField(null=True)
     ceated_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
